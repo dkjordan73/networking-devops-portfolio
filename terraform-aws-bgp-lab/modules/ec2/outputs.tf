731b80662b9a23babb72a46b73e73881fac7a034
dkjordan73 <dkjordan73@gmail.com>
2026-07-06 20:21:42 -0400

    Remove terraform-aws-bgp-lab, project being reworked

--- terraform-aws-bgp-lab/modules/ec2/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "onprem_cgw_public_ip" {
-  description = "Public IP of the StrongSwan/FRRouting CGW instance"
-  value       = aws_instance.onprem_cgw.public_ip
-}
-
-output "cloud_test_instance_private_ip" {
-  description = "Private IP of the cloud side test instance"
-  value       = aws_instance.cloud_test.private_ip
-}
